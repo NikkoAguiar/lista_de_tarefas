09b7ee5ab6303bd6be74ee2f2df234f9480f7d89
Nikko <nikoaguiar900@gmail.com>
2023-05-19 17:08:51 -0300

    update

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 $(document).ready(function(){
+    let novoItem = '';
     $('form').on('submit', function(e){
         e.preventDefault();
         const novaTarefa = $('#informe_a_atividade').val();
-        const novoItem = $(`<li style = "display: none">${novaTarefa}</li>`);
-        // $(`<li id="seila">${novaTarefa}</li>`).appendTo(novoItem);
+        novoItem = $(`<li style = "display: none"><img id="icone_rejeitado" style="height: 12px;" src="./images/icon_rejected.png"/>${novaTarefa}</li>`);
         $(novoItem).appendTo('ul');
         $(novoItem).fadeIn(1000);
         $('#informe_a_atividade').val('');
 
         $('li').click(function(){
+            $('img').css("display", "none");
+            novoItem = $(`<img id="icone_aprovado" style="height: 12px;" src="./images/icon_checked.png"/>`);
+            $('#icone_aprovado').remove();
+            $(novoItem).appendTo(this);
             $(this).css("text-decoration", "line-through");
         })
     })
