c909417dc368ad83837e6dc25b2aec6200d6f890
Nikko <nikoaguiar900@gmail.com>
2023-05-19 13:49:52 -0300

    JavaScript Update

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ $(document).ready(function(){
     $('form').on('submit', function(e){
         e.preventDefault();
         const novaTarefa = $('#informe_a_atividade').val();
-        const novoItem = $('<li style = "display: none"></li>');
-        $(`<li id="seila">${novaTarefa}</li>`).appendTo(novoItem);
+        const novoItem = $(`<li style = "display: none">${novaTarefa}</li>`);
+        // $(`<li id="seila">${novaTarefa}</li>`).appendTo(novoItem);
         $(novoItem).appendTo('ul');
         $(novoItem).fadeIn(1000);
         $('#informe_a_atividade').val('');
